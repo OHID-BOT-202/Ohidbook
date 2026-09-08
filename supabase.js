fb054af67933a8d5da8213d0ded1494894b17012
OHID-BOT-202 <amarnamohid@gmail.com>
2026-09-09 05:01:54 +0600

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,9 +1,9 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
 // এখানে তোমার Supabase information বসাও
-const SUPABASE_URL = "YOUR_SUPABASE_URL";
+const SUPABASE_URL = "https://wbevujnminvgvwkrkysa.supabase.co";
 
-const SUPABASE_KEY = "YOUR_SUPABASE_PUBLISHABLE_KEY";
+const SUPABASE_KEY = "sb_publishable_3D-903R2J0_KJA7LIiyYeQ_kYmIKq_c";
 
 export const supabase = createClient(
   SUPABASE_URL,
